69710fe1f6447e42553a1c554a95c51afbfc14e7
Keisuke Urakari <cxj16662@nifty.com>
2021-10-14 08:04:47 +0900

    ft2

--- lib/test/test-freetype.cpp
+++ lib/test/test-freetype.cpp
@@ -23,20 +23,21 @@ void print(const std::vector<std::vector<bool>> &rows) {
 
 TEST_F(FreetypeTest, test) {
     const auto u32str = FreeType::char2u32str("あ");
-    //    auto ft = FreeType("/usr/share/fonts/truetype/fonts-japanese-mincho.ttf");
-    auto ft = FreeType("/usr/share/fonts/google-noto-cjk/NotoSansCJK-Bold.ttc");
+    auto ft = FreeType("/usr/share/fonts/truetype/fonts-japanese-mincho.ttf");
+    //auto ft = FreeType("/usr/share/fonts/google-noto-cjk/NotoSansCJK-Bold.ttc");
 
     //    const auto &dot = ft.draw("１"); // 12 4
     //    const auto &dot = ft.draw("２");    // 11 8
-    const auto sz = int(32);
-    const auto &ftbitmaps = ft.draw("１あｗΜｇ", sz);
+    const auto sz = int(48);
+
+    const auto &ftbitmaps = ft.draw("１あｗΜｇ！", sz);
     //    const auto &ftbitmaps = ft.draw("１あｗΜ", sz);
 
     for(const auto &fb : ftbitmaps) {
         std::cout << "h: " << fb.height << " w: " << fb.width << std::endl;
         std::cout << "ax: " << fb.advanceX << " ay : " << fb.advanceY << std::endl;
         std::cout << "left: " << fb.left << " top : " << fb.top << std::endl;
-        std::cout << "decender: " << fb.decender <<  " desc: " << fb.desc << " asc: " << fb.asc <<  std::endl;
+        std::cout << "decender: " << fb.decender << " desc: " << fb.desc << " asc: " << fb.asc << std::endl;
 
         print(fb.rows);
     }
@@ -52,12 +53,19 @@ TEST_F(FreetypeTest, test) {
     for(const auto &fb : ftbitmaps) {
         width += fb.advanceX;
     }
-    const auto height = ascMax + descMax;
+    //    const auto height = ascMax + descMax;
+    const auto height = sz;
 
     auto out = png::image<png::rgba_pixel>(width, height);
 
+    const auto &a = ft.draw("あ", sz)[0];
+    const auto asc = a.asc;
+    const auto desc = a.desc;
+
+    const auto top = int((height - (asc + desc)) / 2);
+    const auto base = top + asc;
+    const auto bottom = top + (asc + desc);
     auto left = int(0);
-    auto top = int(0);
 
     for(const auto &fb : ftbitmaps) {
         for(decltype(fb.rows.size()) y = 0; y < fb.rows.size(); ++y) {
@@ -66,8 +74,7 @@ TEST_F(FreetypeTest, test) {
                 const auto red = png::basic_rgba_pixel<uint8_t>(uint8_t(255), uint8_t(0), uint8_t(0));
                 const auto black = png::basic_rgba_pixel<uint8_t>(uint8_t(0), uint8_t(0), uint8_t(0));
                 const auto &color = row[x] ? red : black;
-                //                const auto yy = top + y + (height - fb.top) ;// + fb.decender;
-                const auto yy = top + y + (height - fb.top) - descMax;
+                const auto yy = top + y + (height - top - fb.top) - descMax;
                 const auto xx = left + x + fb.left;
                 if(yy >= 0 && yy < height && xx >= 0 && xx < width) {
                     out[yy][xx] = color;
@@ -80,8 +87,9 @@ TEST_F(FreetypeTest, test) {
     const auto white = png::basic_rgba_pixel<uint8_t>(uint8_t(255), uint8_t(255), uint8_t(255));
     const auto blue = png::basic_rgba_pixel<uint8_t>(uint8_t(0), uint8_t(0), uint8_t(255));
     for(int x = 0; x < width; ++x) {
-        out[ascMax][x] = white;
-        //        out[sz][x] = blue;
+        out[base][x] = white;
+        out[top][x] = blue;
+        out[bottom][x] = blue;
     }
 
     out.write("/tmp/output.png");
