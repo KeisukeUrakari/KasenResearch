c96486d3af9a6328b415e0fc92ea581b63e374d7
Keisuke Urakari <urakari@benest.co.jp>
2021-11-11 10:00:26 +0900

    if

---
+++ lib/test/test-if.cpp
@@ -0,0 +1,42 @@
+#include <boost/log/trivial.hpp>
+#include <gtest/gtest.h>
+
+class IFTest : public ::testing::Test {
+  protected:
+    virtual void SetUp() {
+    }
+
+    // virtual void TearDown() {}
+};
+
+class Interface {
+  public:
+    virtual void func() = 0;
+};
+
+class Impl : public Interface {
+  public:
+    Impl() {
+        BOOST_LOG_TRIVIAL(trace) << "Impl const";
+    }
+
+    ~Impl() {
+        BOOST_LOG_TRIVIAL(trace) << "Impl dest";
+    }
+
+    void func() override {
+        BOOST_LOG_TRIVIAL(trace) << "func";
+    }
+};
+
+TEST_F(IFTest, test) {
+    auto pi = std::shared_ptr<Interface>(nullptr);
+    {
+        auto ptr = std::make_shared<Impl>();
+        {
+            auto ptr2 = ptr;
+        }
+        pi = ptr;
+    }
+    BOOST_LOG_TRIVIAL(trace) << "dbg";
+}
