e2b0efee5a7af676de413fba84261450249a5c7d
Keisuke Urakari <urakari@benest.co.jp>
2021-12-10 19:04:41 +0900

    beast

--- beast/main.cpp
+++ beast/main.cpp
@@ -1,84 +1,104 @@
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
-#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/bind.hpp>
 #include <boost/config.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <string>
- 
+
+#if 0 
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
- 
-void fail(beast::error_code ec, char const* what)
-{
-    std::cerr << what << ": " << ec.message() << "\n";
-}
- 
-void do_session(tcp::socket& socket)
-{
+#else
+namespace beast = boost::beast;
+using tcp = boost::asio::ip::tcp;
+#endif
+
+void do_session(tcp::socket &socket) {
     beast::error_code ec;
- 
+
     beast::flat_buffer buffer;
-    http::request<http::string_body> req;
-    http::read(socket, buffer, req, ec);
-    if(ec)
-        return fail(ec, "read");
- 
-    http::response<http::empty_body> res{http::status::ok, 11};
-    res.keep_alive(false);
+    beast::http::request<beast::http::string_body> req;
+    beast::http::read(socket, buffer, req, ec);
+    if(ec) {
+        //        return fail(ec, "read");
+        throw std::runtime_error(ec.message());
+    }
+
+    std::cout << "method: " << req.method() << " path: " << req.target() << std::endl;
+
+    if(req.method() != boost::beast::http::verb::get) {
+        return;
+    }
+
+    beast::http::response<beast::http::string_body> res{beast::http::status::ok, req.version()};
+    res.set(beast::http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(beast::http::field::content_type, "application/json");
+    res.body() = "{\"key1\":\"value2\"}";
+    res.keep_alive(req.keep_alive());
     res.prepare_payload();
-    http::serializer<false, http::empty_body> sr{res};
-    http::write(socket, sr, ec);
-    if(ec)
-        return fail(ec, "write");
- 
+    beast::http::serializer<false, beast::http::string_body> sr{res};
+    beast::http::write(socket, sr, ec);
+
+    if(ec) {
+        throw std::runtime_error(ec.message());
+    }
+
     socket.shutdown(tcp::socket::shutdown_send, ec);
 }
- 
-int main(int argc, char* argv[])
-{
-    try
-    {
+
+class HttpListener {
+    boost::asio::io_context &io_context_;
+    tcp::acceptor acceptor_;
+
+  public:
+    HttpListener(boost::asio::io_context &io_context, const tcp::endpoint &endpoint)
+        : io_context_(io_context),
+          acceptor_(io_context_, endpoint) {
+              ;
+          }
+
+    void start_accept() {
+        auto socket = std::make_shared<tcp::socket>(io_context_);
+        acceptor_.async_accept(
+            *socket,
+            [this, socket](const boost::system::error_code &error) {
+                if(error) {
+//                    std::cout << "accept failed: " << error.message() << std::endl;
+                } else {
+//                    std::cout << "accept correct!" << std::endl;
+                    do_session(*socket);
+                }
+                start_accept();
+            });
+    }
+};
+
+int main(int argc, char *argv[]) {
+    try {
         // Check command line arguments.
-        if (argc != 3)
-        {
-            std::cerr <<
-                "Usage: http-server-sync <address> <port>\n" <<
-                "Example:\n" <<
-                "    http-server-sync 0.0.0.0 8080\n";
+        if(argc != 3) {
+            std::cerr << "Usage: http-server-sync <address> <port>\n"
+                      << "Example:\n"
+                      << "    http-server-sync 0.0.0.0 8080\n";
             return EXIT_FAILURE;
         }
-        auto const address = net::ip::make_address(argv[1]);
+        auto const address = boost::asio::ip::make_address(argv[1]);
         auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
- 
+
         // The io_context is required for all I/O
-        net::io_context ioc{1};
- 
-        // The acceptor receives incoming connections
-        tcp::acceptor acceptor{ioc, {address, port}};
-        for(;;)
-        {
-            // This will receive the new connection
-            tcp::socket socket{ioc};
- 
-            std::cout << "dbg1" << std::endl;
+        boost::asio::io_context ioc{1};
 
-            // Block until we get a connection
-            acceptor.accept(socket);
- 
-            std::cout << "dbg2" << std::endl;
+        auto listener = HttpListener(ioc, {address, port});
+        listener.start_accept();
+        ioc.run();
 
-            // Launch the session
-            do_session(socket);
-
-            std::cout << "dbg3" << std::endl;
-        }
-    }
-    catch (const std::exception& e)
-    {
+    } catch(const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
